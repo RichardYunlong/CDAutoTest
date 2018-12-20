2fa23673a082f8d11c2c32d9db521147230a7679
RichardYunlong <hewei200685@hotmail.com>
2018-12-20 17:33:22 +0800

    补充注释

--- CDAutoTest/src/AutoTest/GCert.java
+++ CDAutoTest/src/AutoTest/GCert.java
@@ -7,42 +7,175 @@ import java.lang.reflect.Field;
  */
 public class GCert {
 
-	public static String gApplication = "";// 一类应用OU
-	public static String gApplication2 = "";// 二类应用名称
-	public static String gOrganizationID = "";// 机构：证书所属机构
-	public static String gPeriod = ""; // 有效期
-	public static String gTemplateID = "";// 证书模板号
-	public static String gTemplateID2 = "";// 二类证书模板号
-	public static String gCertLevel = "";// 证书等级
-	public static String gKeyAlg = "";// 密钥算法
-	public static Integer gKeyLength = 0;// 密钥长度
-	public static String gSysIdentification = "";// 系统标识（DN规则为5.1时的输入项）
-	public static String gSubscriberIdentification = ""; // 用户唯一标识（DN规则为5.1时的输入项）
-	public static String gExtensionInfo = "";// 自定义扩展域 或 服务器证书主域名之外的域名，中间以[,]分隔
-	public static String gSslType = "";// 服务器证书类型：1-普通，2-多域名，3-带通配符
-	public static String gEmailInCert = "";// 安全邮件证书输入项，安全邮件地址(E)
-	public static String gStreetAddress = "";// 营业地点，EV证书中的STREET=
-	public static String gPostalCode = "";// 营业地点邮政编码，EV证书中的PostalCode=
-	public static String gBusinessCategory = ""; // 业务类型，EV证书中的2.5.4.15=
+	/**
+	 *  一类应用OU
+	 */
+	public static String gApplication = "";
+	
+	/**
+	 *  二类应用名称
+	 */
+	public static String gApplication2 = "";
+	
+	/**
+	 *  证书所属机构
+	 */
+	public static String gOrganizationID = "";
+	
+	/**
+	 *  有效期
+	 */
+	public static String gPeriod = "";
+	
+	/**
+	 *  证书模板号
+	 */
+	public static String gTemplateID = "";
+	
+	/**
+	 *  二类证书模板号
+	 */
+	public static String gTemplateID2 = "";
+	
+	/**
+	 *  证书等级
+	 */
+	public static String gCertLevel = "";
+	
+	/**
+	 *  密钥算法
+	 */
+	public static String gKeyAlg = "";
+	
+	/**
+	 *  密钥长度
+	 */
+	public static Integer gKeyLength = 0;
+	
+	/**
+	 *  系统标识（DN规则为5.1时的输入项）
+	 */
+	public static String gSysIdentification = "";
+	
+	/**
+	 *  用户唯一标识（DN规则为5.1时的输入项）
+	 */
+	public static String gSubscriberIdentification = "";
+	
+	/**
+	 *  自定义扩展域 或 服务器证书主域名之外的域名，中间以[,]分隔
+	 */
+	public static String gExtensionInfo = "";
+	
+	/**
+	 *  服务器证书类型：1-普通，2-多域名，3-带通配符
+	 */
+	public static String gSslType = "";
+	
+	/**
+	 *  安全邮件证书输入项，安全邮件地址(E)
+	 */
+	public static String gEmailInCert = "";
+	
+	/**
+	 *  营业地点，EV证书中的STREET=
+	 */
+	public static String gStreetAddress = ""; 
+	
+	/**
+	 *  营业地点邮政编码，EV证书中的PostalCode=
+	 */
+	public static String gPostalCode = "";
+	
+	/**
+	 *  业务类型，EV证书中的2.5.4.15=
+	 */
+	public static String gBusinessCategory = ""; 
 
-	public static String gSubjectDN = "";// 目标证书DN
-	public static String gSerialNo = "";// 目标证书序列号
-	public static String gAuthCode = "";// 目标证书授权码
-	public static String gCsr = "";// P10申请书
-	public static String gCsrSub = "";// P10复合申请书
-	public static String gCertType = ""; // 证书类型
-	public static String gCertStatus = "";// 证书状态（多种状态以逗号[,]分隔）
-	public static String gNotBeforeFrom = ""; // 生效日期（从）YYYY-MM-DD
-	public static String gNotBeforeTo = "";// 生效日期（到）YYYY-MM-DD
-	public static String gNotAfterFrom = "";// 截止日期（从）YYYY-MM-DD
-	public static String gNotAfterTo = "";// 截止日期（到）YYYY-MM-DD
-	public static String gNotAfter = "";// 截止日期（到）YYYY-MM-DD 用于更新或换发
+	/**
+	 *  目标证书DN
+	 */
+	public static String gSubjectDN = "";
+	
+	/**
+	 *  目标证书序列号
+	 */
+	public static String gSerialNo = "";
+	
+	/**
+	 *  目标证书授权码
+	 */
+	public static String gAuthCode = "";
+	
+	/**
+	 *  P10申请书
+	 */
+	public static String gCsr = "";
+	
+	/**
+	 *  P10复合申请书
+	 */
+	public static String gCsrSub = "";
+	
+	/**
+	 *  证书类型
+	 */
+	public static String gCertType = "";
+	
+	/**
+	 *  证书状态（多种状态以逗号[,]分隔）
+	 */
+	public static String gCertStatus = "";
+	
+	/**
+	 *  生效日期（从）YYYY-MM-DD
+	 */
+	public static String gNotBeforeFrom = "";
+	
+	/**
+	 *  生效日期（到）YYYY-MM-DD
+	 */
+	public static String gNotBeforeTo = "";
+	
+	/**
+	 *  截止日期（从）YYYY-MM-DD
+	 */
+	public static String gNotAfterFrom = "";
+	
+	/**
+	 *  截止日期（到）YYYY-MM-DD
+	 */
+	public static String gNotAfterTo = "";
+	
+	/**
+	 *  截止日期（到）YYYY-MM-DD 用于更新或换发
+	 */
+	public static String gNotAfter = "";
 
-	public static String gKeyID = "";// 预植证书KeyID
-	public static String gRefNo = "";// 预植证书序列号
-	public static String gCert2keyAlg = "";// 附加证书秘钥算法
-	public static String gCert2keyLength = "";// 附加证书秘钥长度
-	public static String gCert2keyLevel = "";// 附加证书证书等级
+	/**
+	 *  预植证书KeyID
+	 */
+	public static String gKeyID = "";
+	
+	/**
+	 *  预植证书序列号
+	 */
+	public static String gRefNo = "";
+	
+	/**
+	 *  附加证书秘钥算法
+	 */
+	public static String gCert2keyAlg = "";
+	
+	/**
+	 *  附加证书秘钥长度
+	 */
+	public static String gCert2keyLength = ""; 
+	
+	/**
+	 *  附加证书证书等级
+	 */
+	public static String gCert2keyLevel = "";
 
 	/**
 	 *  显示类成员变量
