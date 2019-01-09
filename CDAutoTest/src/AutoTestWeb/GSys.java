0ac4ff4697a84a179232d3ba1ecd50d6aec568f6
RichardYunlong <hewei200685@hotmail.com>
2019-01-09 12:42:37 +0800

    修改系统初始化序列

--- CDAutoTest/src/AutoTestWeb/GSys.java
+++ CDAutoTest/src/AutoTestWeb/GSys.java
@@ -24,6 +24,21 @@ public class GSys {
 		new GParam();
 		GLog.GLogDoReady(startTime, "GParam");
 		
+		// 初始化用例管理器;
+		startTime = System.currentTimeMillis();
+		new GTestCase();
+		GLog.GLogDoReady(startTime, "GTestCase");
+		
+		// 初始化用例输出路径;
+		startTime = System.currentTimeMillis();
+		new GOutPutCtrl();
+		GLog.GLogDoReady(startTime, "GOutPutCtrl");
+		
+		// 重写webdriver基础方法;
+		startTime = System.currentTimeMillis();
+		new GWCtrl();
+		GLog.GLogDoReady(startTime, "GWCtrl");
+		
 		// 加载浏览器设置;
 		startTime = System.currentTimeMillis();
 		new GBrowser();
