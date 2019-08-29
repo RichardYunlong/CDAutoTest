753cbf6952f65b540b641cdc9dfe1774c3133eb0
RichardYunlong <hewei200685@hotmail.com>
2019-08-29 18:07:03 +0800

    增加生成随机字符串的方法

--- src/AutoTest/GValue.java
+++ src/AutoTest/GValue.java
@@ -1,5 +1,7 @@
 package AutoTest;
 
+import java.util.Random;
+
 /**
  *  边界值
  */
@@ -7,6 +9,11 @@ public class GValue {
 	private GValue(){
 		GLog.logShowConsole("This is a tool class.");
 	}
+	
+	/**
+	 *  随机字符串组成元素
+	 */
+	public static final String CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
 	/**
 	 *  预签合同原文错误临时值
@@ -14,7 +21,7 @@ public class GValue {
 	public static final String SIGNATTR_ERR = "MWkwGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTcwMzA5MDkxOTU1WjAvBgkqhkiG9w0BCQQxIgQgmn5Zzs0uRe+SFcFFHcnW50q1DDF26GscX3lrohr1mMw=";
 
 	/**
-	 *  按照长度值和组成字母获取字符串
+	 *  按照长度值和组成字母获取指定字符串
 	 *  
 	 *  @param length
 	 *  @param tChar
@@ -28,5 +35,22 @@ public class GValue {
 		}
 
 		return randomStringByLength;
+	}
+	
+	/**
+	 *  按照长度值和取值范围获取随机字符串
+	 *  
+	 *  @param length
+	 *  @return 返回目标字符串
+	 */
+	public static String getRandomStringByLength(int length) {		
+		Random random = new Random();
+		StringBuffer sb = new StringBuffer();
+	    for (int i = 0; i < length; i++) {
+	        int number = random.nextInt(CHARS.length());
+	        sb.append(CHARS.charAt(number));
+	    }
+
+		return sb.toString();
 	}
 }
