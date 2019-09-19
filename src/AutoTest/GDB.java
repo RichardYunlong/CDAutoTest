6e4ad74a6bebc23f82fb3e6abfd64da11cbf4e82
RichardYunlong <hewei200685@hotmail.com>
2019-09-19 15:07:01 +0800

    增加数据库操作

--- src/AutoTest/GDB.java
+++ src/AutoTest/GDB.java
@@ -1,229 +1,142 @@
 package AutoTest;
 
-import java.util.Properties;
-
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
-
-import Plugins.CommonUtil;
-import Plugins.SystemConst;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class GDB {
-	private GDB(){
-		GLog.logShowConsole("This is a tool class.");
-	}
-	
-	/**
-	 *  配置文件主体内容
-	 */
-	public static ApplicationContext applicationContext;
-	
-	/**
-	 *  配置文件属性
-	 */
-	private static Properties property;
-	
-	/**
-	 *  数据库驱动名称
-	 */
-	public static String driverClassName = "";
-	
-	/**
-	 *  数据库地址
-	 */
-	public static String url = "";
-	
-	/**
-	 *  用户名
-	 */
-	public static String username = "";
-	
-	/**
-	 *  密码
-	 */
-	public static String password = "";
-	
-	/**
-	 *  检查语句
-	 */
-	public static String validationQuery = "";
-	
-	/**
-	 *  链接与读取超时
-	 */
-	public static String connectTimeoutAndReadTimeout = "";
-	
-	/**
-	 *  连接池初始化连接数量
-	 */
-	public static String initialSize = "";
-	
-	/**
-	 *  连接池中最小的空闲的连接数
-	 */
-	public static String minIdle = "";
-	
-	/**
-	 *  连接池中最大的空闲的连接数 
-	 */
-	public static String maxIdle = "";
-	
-	/**
-	 *  连接池中可同时连接的最大的连接数   
-	 */
-	public static String maxActive = "";
-	
-	/**
-	 *  最大建立连接等待时间,单位为毫秒 
-	 */
-	public static String maxWait = "";
-	
-	/**
-	 *  是否自动提交 
-	 */
-	public static String defaultAutoCommit = "";
-	
-	/**
-	 *  是否自动回收超时连接 
-	 */
-	public static String removeAbandoned = "";
+    /*
+     * 增加
+     */
+    public void add() {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            // 获取连接
+            connection = GDBConnector.getConnection();
 
-	/**
-	 *  超时时间(以秒数为单位) 
-	 */
-	public static String removeAbandonedTimeout = "";
-	
-	/**
-	 *  是否在空闲时间测试连接 
-	 */
-	public static String testWhileIdle = "";
-	
-	/**
-	 *  一个连接在池中最小生存的时间,单位为毫秒  
-	 */
-	public static String timeBetweenEvictionRunsMillis = "";
-	
-	/**
-	 *  连接池中连接可空闲的时间,单位为毫秒 
-	 */
-	public static String minEvictableIdleTimeMillis = "";
-	
-	/**
-	 *  每次检查链接的数量  
-	 */
-	public static String numTestsPerEvictionRun = "";
-	
-	/**
-	 *  返回对象时是否进行验证 
-	 */
-	public static String testOnReturn = "";
-	
-	/**
-	 *  取出对象时是否进行验证 
-	 */
-	public static String testOnBorrow = "";
+            String carnumber_head = "甬-K-1F-";
+            for (int i = 1; i <= 100; i++) {
 
-	/**
-	 *  初始化配置文件
-	 *  
-	 *  @param appContext 参数集
-	 */
-	public static void init(ApplicationContext appContext) {
-		applicationContext = appContext;
-		property = (Properties) applicationContext.getBean("property");
-		
-		driverClassName = (String) property.get("db.driverClassName");
-		url = (String) property.get("db.url");
-		username = (String) property.get("db.username");
-		password = (String) property.get("db.password");
-		validationQuery = (String) property.get("db.validationQuery");
-		connectTimeoutAndReadTimeout = (String) property.get("db.connectTimeoutAndReadTimeout");
-		initialSize = (String) property.get("db.initialSize");
-		minIdle = (String) property.get("db.minIdle");
-		maxIdle = (String) property.get("db.maxIdle");
-		maxActive = (String) property.get("db.maxActive");
-		maxWait = (String) property.get("db.maxWait");
-		defaultAutoCommit = (String) property.get("db.defaultAutoCommit");
-		removeAbandoned = (String) property.get("db.removeAbandoned");
-		removeAbandonedTimeout = (String) property.get("db.removeAbandonedTimeout");
-		testWhileIdle = (String) property.get("db.testWhileIdle");
-		timeBetweenEvictionRunsMillis = (String) property.get("db.timeBetweenEvictionRunsMillis");
-		minEvictableIdleTimeMillis = (String) property.get("db.minEvictableIdleTimeMillis");
-		numTestsPerEvictionRun = (String) property.get("db.numTestsPerEvictionRun");
-		testOnReturn = (String) property.get("db.testOnReturn");
-		testOnBorrow = (String) property.get("db.testOnBorrow");
-		
-		if((!driverClassName.equals("")) 
-			&& (!url.equals("")) 
-			&& (!username.equals("")) 
-			&& (!password.equals("")) 
-			&& (!validationQuery.equals("")) 
-			&& (!connectTimeoutAndReadTimeout.equals("")) 
-			&& (!initialSize.equals("")) 
-			&& (!minIdle.equals("")) 
-			&& (!maxIdle.equals("")) 
-			&& (!maxActive.equals("")) 
-			&& (!maxWait.equals(""))
-			&& (!defaultAutoCommit.equals("")) 
-			&& (!removeAbandoned.equals("")) 
-			&& (!removeAbandonedTimeout.equals("")) 
-			&& (!testWhileIdle.equals("")) 
-			&& (!timeBetweenEvictionRunsMillis.equals("")) 
-			&& (!minEvictableIdleTimeMillis.equals("")) 
-			&& (!numTestsPerEvictionRun.equals("")) 
-			&& (!testOnReturn.equals("")) 
-			&& (!testOnBorrow.equals("")) 
-		) {
-			
-		}else {
-			GSys.logErrorSys("One of these db params from config may has no value, Please check again!");
-			System.exit(0);
-		}
-	}
+                if (i < 10) {
+//                  System.out.println("value------>>>" + carnumber_head+"000" + i);
+                     // 准备sql语句
+                    String sql = "INSERT INTO cx_carnumberpool(carnumber) VALUES(?)";
+                    // 获取PrepareStatement对象
+                    preparedStatement = connection.prepareStatement(sql);
+                    // 填充占位符
+                    preparedStatement.setString(1, carnumber_head+"000" + i);
+                    int num = preparedStatement.executeUpdate();// 返回影响到的行数
 
-	/**
-	 *  获得配置文件主体内容
-	 *  
-	 *  @return 返回参数集
-	 */
-	public static ApplicationContext getApplicationContext() {
-		return applicationContext;
-	}
+                    System.out.println("一共影响到" + num + "行");
 
-	/**
-	 *  获得配置文件处理类
-	 *  
-	 *  @param name 参数集名称
-	 *  @return 参数集类
-	 */
-	public static Object getBean(String name) {
-		return applicationContext.getBean(name);
-	}
+                } else if (i < 100) {
+                     // 准备sql语句
+                    String sql = "INSERT INTO cx_carnumberpool(carnumber) VALUES(?)";
+                    // 获取PrepareStatement对象
+                    preparedStatement = connection.prepareStatement(sql);
+                    // 填充占位符
+                    preparedStatement.setString(1, carnumber_head+"00" + i);
+                    int num = preparedStatement.executeUpdate();// 返回影响到的行数
 
-	/**
-	 *  获得配置文件属性
-	 *  
-	 *  @return 属性值
-	 */
-	public static Properties getProperty() {
-		return property;
-	}
-	
-	/**
-	 *  配置文件驱动构造
-	 */
-	public static void loadConfig() {
-		try {
-			String configLocation = "config";
-			System.setProperty(SystemConst.CONFIG_LOCATION, configLocation);
+                    System.out.println("一共影响到" + num + "行");
+                } else if (i < 1000) {
+                     // 准备sql语句
+                    String sql = "INSERT INTO cx_carnumberpool(carnumber) VALUES(?)";
+                    // 获取PrepareStatement对象
+                    preparedStatement = connection.prepareStatement(sql);
+                    // 填充占位符
+                    preparedStatement.setString(1, carnumber_head+"0" + i);
+                    int num = preparedStatement.executeUpdate();// 返回影响到的行数
+                    System.out.println("一共影响到" + num + "行");
+                } 
 
-			String springConfigFile = CommonUtil.getConfigPath() + SystemConst.SPRING_CONFIG_FILE;
-			ApplicationContext appContext = new FileSystemXmlApplicationContext(springConfigFile);
-			init(appContext);
-		}catch(Exception e){
-			GSys.logErrorSys(GMsg.MSG_IOFAILED[0]);
-			e.printStackTrace();
-			System.exit(0);
-		}
-	}
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+        	GDBConnector.close(connection, preparedStatement, null);
+        }
+    }
+
+    /*
+     * 读取查询
+     */
+    public void retrieve() {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        try {
+            connection = GDBConnector.getConnection();
+            String sql = "SELECT user_name,user_password,user_birth FROM user";
+            preparedStatement = connection.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+
+            // 遍历结果集
+            while (resultSet.next()) {
+                String username = resultSet.getString(1);
+                String password = resultSet.getString(2);
+                Date userbirth = resultSet.getDate(3);
+
+                System.out.println(username + ":" + password + ":" + userbirth);
+            }
+        } catch (Exception e) {
+            // TODO: handle exception
+        } finally {
+        	GDBConnector.close(connection, preparedStatement, resultSet);
+        }
+
+    }
+
+    /*
+     * 修改更新
+     */
+    public void update() {
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = GDBConnector.getConnection();
+            String sql = "UPDATE USER SET user_password = ? WHERE user_name = ?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, "passwordupdate");
+            preparedStatement.setString(2, "mary");
+            int num = preparedStatement.executeUpdate();
+
+            System.out.println("一共影响到" + num + "行");
+        } catch (Exception e) {
+            // TODO: handle exception
+        } finally {
+        	GDBConnector.close(connection, preparedStatement, null);
+        }
+    }
+
+    /*
+     * 删除
+     */
+    public void delete() {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = GDBConnector.getConnection();
+            String sql = "DELETE FROM user WHERE user_id = ?";
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setInt(1, 3);
+            int num = preparedStatement.executeUpdate();
+
+            System.out.println("一共影响到" + num + "行");
+        } catch (Exception e) {
+            // TODO: handle exception
+        } finally {
+        	GDBConnector.close(connection, preparedStatement, null);
+        }
+
+    }
+
+    public static void main(String[] args) {
+         GDB test = new GDB();
+         test.add();
+    }
 }
---
+++ src/AutoTest/GDBConnector.java
@@ -0,0 +1,72 @@
+package AutoTest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class GDBConnector {
+    /** 
+     * 获得数据库连接 
+     *  
+     * @return conn
+     */  
+    public static Connection getConnection() {  
+        Connection conn = null;  
+        String driverName = "oracle.jdbc.OracleDriver";  
+        String url = "jdbc:oracle:thin:@192.168.115.90:1521:orcl";  
+        String username = "AutoTest";  
+        String password = "AutoTest";  
+  
+        try {  
+            Class.forName(driverName);  
+            conn = DriverManager.getConnection(url, username, password);  
+        } catch (ClassNotFoundException e) {  
+            System.out.println("FAIL TO LOAD DB DRIVER");  
+            e.printStackTrace();  
+        } catch (SQLException e) {  
+            System.out.println("FAIL TO GET DB CONNECTOR");  
+            e.printStackTrace();  
+        }  
+        return conn;  
+    }  
+  
+    /** 
+     * 关闭数据库连接 
+     *  
+     * @param conn 
+     * @param ps 
+     * @param rs 
+     */  
+    public static void close(Connection conn, PreparedStatement ps, ResultSet rs) {  
+        // 关闭原则:先开后关,后开先关  
+        if (rs != null) {  
+            try {  
+                rs.close();  
+            } catch (SQLException e) {  
+                e.printStackTrace();  
+            }  
+        }  
+  
+        if (ps != null) {  
+            try {  
+                ps.close();  
+            } catch (SQLException e) {  
+                e.printStackTrace();  
+            }  
+        }  
+  
+        if (conn != null) {  
+            try {  
+                conn.close();  
+            } catch (SQLException e) {  
+                e.printStackTrace();  
+            }  
+        }  
+    }  
+  
+//    public static void main(String[] args) {  
+//        System.out.println(getConnection());  
+//    }  
+}
