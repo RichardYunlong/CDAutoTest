c69109b2de67994eda399ed71bb7b80848f8d1b7
RichardYunlong <hewei200685@hotmail.com>
2019-10-10 16:23:50 +0800

    升级至3.0.7.1

---
+++ src/AutoTest/GApplication.java
@@ -0,0 +1,85 @@
+package AutoTest;
+
+/**
+ *  应用信息
+ */
+public class GApplication {
+	
+	/**
+	 *  一类应用名称
+	 */
+	public static String gApplicationName = "";
+	
+	/**
+	 *  一类应用OU
+	 */
+	public static String gApplicationOU = "";
+	
+	/**
+	 *  一类应用CA名称
+	 */
+	public static String gApplicationCAName = "";
+	
+	/**
+	 *  一类应用系统标识（DN规则为5.1时的输入项）
+	 */
+	public static String gApplicationSysIdent = "";
+	
+	/**
+	 *  二类应用名称
+	 */
+	public static String gApplication2Name = "";
+	
+	/**
+	 *  二类应用OU
+	 */
+	public static String gApplication2OU = "";
+	
+	/**
+	 *  二类应用CA名称
+	 */
+	public static String gApplication2CAName = "";
+	
+	/**
+	 *  二类应用系统标识（DN规则为5.1时的输入项）
+	 */
+	public static String gApplication2SysIdent = "";
+	
+	/**
+	 *  三类类应用名称
+	 */
+	public static String gApplication3Name = "";
+	
+	/**
+	 *  三类应用OU
+	 */
+	public static String gApplication3OU = "";
+	
+	/**
+	 *  三类应用CA名称
+	 */
+	public static String gApplication3CAName = "";
+	
+	/**
+	 *  三类应用系统标识（DN规则为5.1时的输入项）
+	 */
+	public static String gApplication3SysIdent = "";
+	
+	/**
+	 * @see 重置用户参数
+	 */
+	public static void reSetGApplication() {
+		gApplicationName = "";
+		gApplicationOU = "";
+		gApplicationCAName = "";
+		gApplicationSysIdent = "";
+		gApplication2Name = "";
+		gApplication2OU = "";
+		gApplication2CAName = "";
+		gApplication2SysIdent = "";
+		gApplication3Name = "";
+		gApplication3OU = "";
+		gApplication3CAName = "";
+		gApplication3SysIdent = "";
+	}
+}
---
+++ src/AutoTest/GOrder.java
@@ -0,0 +1,62 @@
+package AutoTest;
+
+/**
+ *  订单信息
+ */
+public class GOrder {
+	
+	/**
+	 *  订单详情
+	 */
+	public static String gOrderDetail = "";
+	
+	/**
+	 *  订单编码
+	 */
+	public static String gOrderId = "";
+	
+	/**
+	 *  办理订单号
+	 */
+	public static String gHandleOrderNo = "";
+	
+	/**
+	 *  流水号
+	 */
+	public static String gFlowNo = "";
+	
+	/**
+	 *  单个订单内相同商品数量
+	 */
+	public static String  gApplyCourt = "";
+	
+	/**
+	 *  支付返回地址
+	 */
+	public static String  gPayUrl;
+	
+	/**
+	 *  支付交易码
+	 */
+	public static String  gPaymentCode = "";
+	
+	/**
+	 *  支付交易信息
+	 */
+	public static String  gPaymentMsg = "";
+	
+	/**
+	 *  支付签名值
+	 */
+	public static String  gPaymentSign = "";
+	
+	/**
+	 *  是否需要纸质发票
+	 */
+	public static String  gIsPaperInvoice ;
+	
+	/**
+	 *  发票号
+	 */
+	public static String  gInvoiceNo ;
+}
