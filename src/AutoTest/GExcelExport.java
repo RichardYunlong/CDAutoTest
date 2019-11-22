f31f4b02107c224ace8cb37a5014c7c1f64cae30
RichardYunlong <hewei200685@hotmail.com>
2019-11-22 17:34:52 +0800

    修改bug

--- src/AutoTest/GExcelExport.java
+++ src/AutoTest/GExcelExport.java
@@ -299,12 +299,12 @@ public class GExcelExport {
 				reportVO.setCaseScription(GProgress.strTestCaseInputArray[i][2]);
 				reportVO.setPrefixCondition(GProgress.strTestCaseInputArray[i][3]);
 				reportVO.setCaseStep(GProgress.strTestCaseInputArray[i][4]);
-				reportVO.setOutputMix("ResultCode:" + GResult.strResultTSNO[i][0] + ";ResultMessage:" + GResult.strResultTSNO[i][1]);
+				reportVO.setOutputMix("ResultCode:" + GResult.strResultTSNO[i-1][0] + ";ResultMessage:" + GResult.strResultTSNO[i-1][1]);
 				reportVO.setOutputMix1("与预期一致");
 				reportVO.setOutputMix2("");
-				reportVO.setIsPassed(GResult.strResultTSNO[i][2]);
+				reportVO.setIsPassed(GResult.strResultTSNO[i-1][2]);
 				reportVO.setCaseKind("接口测试");
-				reportVO.setCasePriority(GResult.strResultTSNO[i][4]);
+				reportVO.setCasePriority(GResult.strResultTSNO[i-1][4]);
 				reportVO.setCaseMark("");
 				doExportExcelByLine(reportVO);
 				
