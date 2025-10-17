b665962891feef176a07e9aaf0067c3e1ed10151
RichardYunlong <hewei200685@hotmail.com>
2025-10-17 18:10:52 +0800

    增加本地常驻进程逻辑

---
+++ cdtestapp startup.bat
@@ -0,0 +1,14 @@
+@echo off
+cls
+
+setlocal EnableDelayedExpansion
+
+set lib=.\lib
+set classpath=.
+
+FOR /R %lib% %%f IN (*.jar) DO (
+    set classpath=!classpath!;"%%f"
+)
+
+"%JAVA_HOME%\bin\java" -server -Dfile.encoding=UTF-8 -Xms1024M -Xmx1024M -classpath %classpath% App.cdtestapp -start 3600
+pause
